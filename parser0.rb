022a14e474212ea2314751516b29b2e5a0caaa34
AliRezaTaleghani <shayne.alone@gmail.com>
2013-05-24 00:20:32 +0430

    Stage0

---
+++ parser0.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'mongo'
+require 'bson'
+#require 'trie'
+#require 'awesome_print'
+include Mongo
+@database = MongoClient.new('localhost').db("local")
+@coll = @database.collection("row")
+#################
+def extractElements(legX)
+doc = {"stage" => 0}
+  legX.gsub(/(\w)\(/, "\n[\\1]\(").each_line do |row|
+    case row[1]
+	when "I" then doc [:Ingress] = row
+	when "O" then doc [:Egress] = row
+	when "B" then doc [:elemB] = row
+	when "C" then doc [:elemC] = row
+	when "U" then doc [:elemU] = row
+	when "V" then doc [:elemV] = row
+	when "A" then doc [:elemA] = row
+	when "D" then doc [:elemD] = row
+	when "M" then doc [:elemM] = row
+	when "E" then doc [:elemE] = row
+	when "Y" then doc [:elemY] = row
+	when "Z" then doc [:elemZ] = row
+	else
+	  if row.length > 1
+	    puts row  #doc = {"elemMisc" => "#{row}"} ; coll.update({"_id" => @id},{"$set" => doc})
+	  end
+	end
+    end
+@coll.insert(doc)
+#puts id
+end
+##########
+#reader = `sed 's:,,*:,:g' #{ARGV[0]} | tr -d '\n' | sed 's:I(:\\nI(:g' | sed 's:O(:\\nO(:g'`
+reader = `cat #{ARGV[0]} | tr -d '\n' | sed -e 's:[IOYZ](:\\n&:g'`
+reader.each_line do |row|
+case row[0]
+    when "I" then extractElements row  # ;puts row
+    when "O" then extractElements row  # ;puts row
+    when "Y" then extractElements row  # ;puts row
+    when "Z" then extractElements row  # ;puts row
+    else puts "Misc:\n#{row}\n======="
+  end
+end
---
+++ parser1.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'mongo'
+require 'bson'
+require 'peach'
+require 'redis'
+require 'awesome_print'
+include Mongo
+@database = MongoClient.new('localhost',27017,:pool_size => 30, :pool_timeout => 180).db("local")
+@coll = @database.collection("row")
+cursor=@coll.find({"Ingress" => {'$exists'=>1}})
+  cursor.peach do |row|
+    callid = row['Ingress'].split(',').to_a[1]
+    engine = row['Ingress'].split(',').to_a[7]
+	#puts "In: #{callid}"
+    cmd = {
+        aggregate: 'row',
+	pipeline:[
+		{'$project' => {'Egress' => 1}},
+		{'$match' => {'Egress' => {'$regex' => callid , '$regex' => engine } }},
+            ]
+            }
+	res = @database.command(cmd)['result']
+	res.each do |row|
+	#	puts row['Egress'].split(',').to_a[1]
+	end
+
+	#puts "Engine: #{engine}"
+end
+
+#puts @coll.find({"Egress"=>{'$exists'=>1}}).count()
+#puts row['Ingress']
+#  cmd = {
+#    aggregate: 'carriers',
+#    pipeline: [
+#  {'$project' => {
+#    'rates' => 1,
+#    'Name' => 1}},
+#  {'$unwind' => '$rates'},
+#  {'$match' => {
+#    'rates.Prefix' => "#{dnid[0,dnid.length-count]}",
+#    'rates.Activation' => "2013-04-21"
+#  }},
+#  {'$limit' => 1 },
+# ]}
+# res = @db.command(cmd)['result']
+######################################
+#cursor=@coll.find({"Egress"=>{"$exists"=>1}},:fields => {accountcodes: { '$elemMatch' => { id: accountcode }}})
+#cursor=@coll.find({"Egress"=>{"$exists"=>1}},:fields => {elemA:1,elemD:1})
+#cursor=@coll.find({},:fields => {Ingress:1,Egress:1,elemA:1,elemD:1})
+######################################
