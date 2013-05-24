cf0920c185eb854ba2b3839688f67707f8b97e92
AliRezaTaleghani <shayne.alone@gmail.com>
2013-05-24 11:38:14 +0430

    some minor change added on Egress Verbose log to matching calledID.

--- parser1.rb
+++ parser1.rb
@@ -7,26 +7,35 @@ require 'peach'
 require 'redis'
 require 'awesome_print'
 include Mongo
-@database = MongoClient.new('localhost',27017,:pool_size => 30, :pool_timeout => 180).db("local")
+@database = MongoClient.new('localhost',27017,:pool_size => 1, :pool_timeout => 180).db("local")
 @coll = @database.collection("row")
 cursor=@coll.find({"Ingress" => {'$exists'=>1}})
-  cursor.peach do |row|
-    callid = row['Ingress'].split(',').to_a[1]
-    engine = row['Ingress'].split(',').to_a[7]
-	#puts "In: #{callid}"
-    cmd = {
+  cursor.each do |rowIn|
+    callid = rowIn['Ingress'].split(',').to_a[1]
+    engine = rowIn['Ingress'].split(',').to_a[7]
+	puts "CallID(Ingress): #{callid}"
+######################################
+## Egress lookup query
+        cmd = {
         aggregate: 'row',
 	pipeline:[
-		{'$project' => {'Egress' => 1}},
-		{'$match' => {'Egress' => {'$regex' => callid , '$regex' => engine } }},
+		{'$match' => {'Egress' => {'$regex' => callid } }},
+#		{'$project' => {'Egress' => 1}},
             ]
             }
 	res = @database.command(cmd)['result']
-	res.each do |row|
-	#	puts row['Egress'].split(',').to_a[1]
+######################################
+	res.each do |rowEg|
+		puts "CallID(Egress): #{rowEg['Egress'].split(',').to_a[1]}"
+		puts "\t#{rowIn['_id']}\n\t#{rowEg['_id']}"
+		puts rowEg['elemA'].split('{').to_a[1].split(',').to_a[0]
+		puts rowEg['elemA'].split('{').to_a[2].split(',').to_a[18]
+		puts rowEg['elemA'].split('{').to_a[2].split(',').to_a[20]
+		puts rowEg['elemA'].split('{').to_a[2].split(',').to_a[45]
+		puts rowEg['elemC']#.split(',').to_a[3]
 	end
 
-	#puts "Engine: #{engine}"
+	puts "Engine: #{engine}"
 end
 
 #puts @coll.find({"Egress"=>{'$exists'=>1}}).count()
